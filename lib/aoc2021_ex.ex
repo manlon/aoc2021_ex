459638025cf6d53a7609bc19bc9149b56173b281
Matt Hanlon <matthanlon@gmail.com>
2021-11-30 10:35:18 -0800

    Initial commit

---
+++ lib/aoc2021_ex.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2021Ex do
+  @moduledoc """
+  Documentation for `Aoc2021Ex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2021Ex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2021_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2021ExTest do
+  use ExUnit.Case
+  doctest Aoc2021Ex
+
+  test "greets the world" do
+    assert Aoc2021Ex.hello() == :world
+  end
+end
