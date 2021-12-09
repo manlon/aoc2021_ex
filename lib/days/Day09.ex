c589f0509925b70eaa9b511d701c1910416d27b0
Matt Hanlon <matthanlon@gmail.com>
2021-12-09 11:07:33 -0800

    day 9 golf

--- lib/days/Day09.ex
+++ lib/days/Day09.ex
@@ -3,46 +3,35 @@ defmodule Aoc2021Ex.Day09 do
 
   def solve1 do
     map = input_map()
-
-    Enum.map(lowpoints(map), fn pos -> Map.get(map, pos) + 1 end)
-    |> Enum.sum()
+    Enum.sum(Enum.map(lowpoints(map), &(Map.get(map, &1) + 1)))
   end
 
   def solve2 do
     map = input_map()
 
-    Enum.map(lowpoints(map), fn pt -> length(expand_basin(map, [pt])) end)
+    Enum.map(lowpoints(map), fn pt -> length(expand_basin(map, [pt], [])) end)
     |> Enum.sort(:desc)
     |> Enum.take(3)
     |> Enum.product()
   end
 
   def lowpoints(map) do
-    Enum.filter(Map.keys(map), fn pos -> is_lowpoint?(map, pos) end)
+    Map.keys(map)
+    |> Enum.filter(fn pos -> Enum.all?(neighbors(map, [pos]), &(map[pos] < map[&1])) end)
   end
 
-  def is_lowpoint?(map, pos) do
-    neighbors(map, pos)
-    |> Enum.all?(fn neighb -> Map.get(map, pos) < Map.get(map, neighb) end)
+  def expand_basin(_, [], basin_points), do: basin_points
+
+  def expand_basin(map, new_points, basin_points) do
+    basin_points = new_points ++ basin_points
+    new_points = Enum.filter(neighbors(map, basin_points), &(Map.get(map, &1) != 9))
+    expand_basin(map, new_points, basin_points)
   end
 
-  def expand_basin(map, basin_points) do
-    new_neighbors = Enum.flat_map(basin_points, fn pt -> neighbors(map, pt) end)
-
-    new_points =
-      (Enum.uniq(new_neighbors) -- basin_points)
-      |> Enum.filter(fn pt -> Map.get(map, pt) != 9 end)
-
-    if new_points == [] do
-      basin_points
-    else
-      expand_basin(map, basin_points ++ new_points)
-    end
-  end
-
-  def neighbors(map, {r, c}) do
-    [{r + 1, c}, {r - 1, c}, {r, c + 1}, {r, c - 1}]
-    |> Enum.filter(fn pos -> Map.has_key?(map, pos) end)
+  def neighbors(map, points) do
+    Enum.flat_map(points, fn {r, c} -> [{r + 1, c}, {r - 1, c}, {r, c + 1}, {r, c - 1}] end)
+    |> Enum.uniq()
+    |> Enum.filter(&(Map.has_key?(map, &1) && &1 not in points))
   end
 
   def input_map do
