9ee2dc0a0b6d0fc4eeeb41e53fae0fa0d54ac1e0
Matt Hanlon <matthanlon@gmail.com>
2021-12-08 23:55:04 -0800

    day 8 simplification

--- lib/days/Day08.ex
+++ lib/days/Day08.ex
@@ -1,18 +1,5 @@
 defmodule Aoc2021Ex.Day08 do
   use Aoc2021Ex.Day
-
-  @segments %{
-    0 => [:a, :b, :c, :e, :f, :g],
-    1 => [:c, :f],
-    2 => [:a, :c, :d, :e, :g],
-    3 => [:a, :c, :d, :f, :g],
-    4 => [:b, :c, :d, :f],
-    5 => [:a, :b, :d, :f, :g],
-    6 => [:a, :b, :d, :e, :f, :g],
-    7 => [:a, :c, :f],
-    8 => [:a, :b, :c, :d, :e, :f, :g],
-    9 => [:a, :b, :c, :d, :f, :g]
-  }
   @seg_counts_uniq %{1 => 2, 4 => 4, 7 => 3, 8 => 7}
 
   def solve1 do
@@ -31,34 +18,18 @@ defmodule Aoc2021Ex.Day08 do
     |> Enum.sum()
   end
 
-  def solve_segment_map(digit_clusters) do
-    [cf, bcdf, acf, abcdefg] =
-      [1, 4, 7, 8]
-      |> Enum.map(fn d ->
-        Enum.find(digit_clusters, fn c -> length(c) == @seg_counts_uniq[d] end)
-      end)
+  def solve_segment_map(digits) do
+    {[d1], digits} = Enum.split_with(digits, fn d -> length(d) == @seg_counts_uniq[1] end)
+    {[d4], digits} = Enum.split_with(digits, fn d -> length(d) == @seg_counts_uniq[4] end)
+    {[d7], digits} = Enum.split_with(digits, fn d -> length(d) == @seg_counts_uniq[7] end)
+    {[d8], digits} = Enum.split_with(digits, fn d -> length(d) == @seg_counts_uniq[8] end)
+    {two_three_five, zero_six_nine} = Enum.split_with(digits, fn d -> length(d) == 5 end)
+    {[d3], two_five} = Enum.split_with(two_three_five, fn d -> d1 -- d == [] end)
+    {zero_nine, [d6]} = Enum.split_with(zero_six_nine, fn d -> d1 -- d == [] end)
+    {[d5], [d2]} = Enum.split_with(two_five, fn d -> d -- d6 == [] end)
+    {[d9], [d0]} = Enum.split_with(zero_nine, fn d -> d -- (d5 ++ d1) == [] end)
 
-    a = acf -- cf
-    two_three_five = Enum.filter(digit_clusters, fn c -> length(c) == 5 end)
-    dg = intersect(two_three_five) -- a
-    g = dg -- bcdf
-    d = dg -- g
-    b = bcdf -- (cf ++ d)
-    e = abcdefg -- (acf ++ b ++ d ++ g)
-    three_six_nine = Enum.filter(digit_clusters, fn c -> length(c) == 6 end)
-
-    f =
-      Enum.map(three_six_nine, fn segs -> segs -- (a ++ b ++ d ++ e ++ g) end)
-      |> Enum.find(fn x -> length(x) == 1 end)
-
-    c = cf -- f
-
-    map = %{a: hd(a), b: hd(b), c: hd(c), d: hd(d), e: hd(e), f: hd(f), g: hd(g)}
-
-    Enum.map(@segments, fn {digit, digit_segs} ->
-      {Enum.sort(Enum.map(digit_segs, fn seg -> Map.get(map, seg) end)), digit}
-    end)
-    |> Map.new()
+    %{d0 => 0, d1 => 1, d2 => 2, d3 => 3, d4 => 4, d5 => 5, d6 => 6, d7 => 7, d8 => 8, d9 => 9}
   end
 
   def parsed_input do
@@ -73,12 +44,5 @@ defmodule Aoc2021Ex.Day08 do
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(&Enum.sort/1)
     |> Enum.map(fn c -> Enum.map(c, &String.to_atom/1) end)
-  end
-
-  def intersect([list]), do: list
-
-  def intersect([l1, l2 | rest]) do
-    filtered = for i <- l1, i in l2, do: i
-    intersect([filtered | rest])
   end
 end
