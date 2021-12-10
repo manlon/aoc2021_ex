740f85f372f139865e7cd8afc30a366c62110049
Matt Hanlon <matthanlon@gmail.com>
2021-12-09 22:42:15 -0800

    day 10 style

--- lib/days/Day10.ex
+++ lib/days/Day10.ex
@@ -2,7 +2,8 @@ defmodule Aoc2021Ex.Day10 do
   use Aoc2021Ex.Day
 
   @illegal_score %{?) => 3, ?] => 57, ?} => 1197, ?> => 25137}
-  @compl_score %{")" => 1, "]" => 2, "}" => 3, ">" => 4}
+  @compl_score %{?) => 1, ?] => 2, ?} => 3, ?> => 4}
+  @flip %{?( => ?), ?[ => ?], ?{ => ?}, ?< => ?>}
 
   def solve1 do
     Enum.sum(for {:invalid, c} <- parsed_input(), do: @illegal_score[c])
@@ -16,17 +17,11 @@ defmodule Aoc2021Ex.Day10 do
   def score_compl([], acc), do: acc
   def score_compl([char | rest], acc), do: score_compl(rest, acc * 5 + @compl_score[char])
 
-  def parsed_input(), do: Enum.map(input_lines(), &parse_line/1)
+  def parsed_input(), do: Enum.map(input_lines(), &parse/1)
 
-  def parse_line(line, acc \\ [])
-  def parse_line("(" <> rest, acc), do: parse_line(rest, [")" | acc])
-  def parse_line("[" <> rest, acc), do: parse_line(rest, ["]" | acc])
-  def parse_line("{" <> rest, acc), do: parse_line(rest, ["}" | acc])
-  def parse_line("<" <> rest, acc), do: parse_line(rest, [">" | acc])
-  def parse_line(")" <> rest, [")" | acc]), do: parse_line(rest, acc)
-  def parse_line("]" <> rest, ["]" | acc]), do: parse_line(rest, acc)
-  def parse_line("}" <> rest, ["}" | acc]), do: parse_line(rest, acc)
-  def parse_line(">" <> rest, [">" | acc]), do: parse_line(rest, acc)
-  def parse_line("", acc), do: {:done, acc}
-  def parse_line(<<c, _::binary>>, _), do: {:invalid, c}
+  def parse(line, acc \\ [])
+  def parse(<<c, rest::binary>>, acc) when c in '([{<', do: parse(rest, [@flip[c] | acc])
+  def parse(<<c, rest::binary>>, [c | acc]), do: parse(rest, acc)
+  def parse(<<c, _::binary>>, _), do: {:invalid, c}
+  def parse("", acc), do: {:done, acc}
 end
