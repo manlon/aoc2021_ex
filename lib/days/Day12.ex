5277cd4a6eb743ee44495bc92f2036d0a1149c7d
Matt Hanlon <matthanlon@gmail.com>
2021-12-12 17:32:16 -0800

    day 12 golf

--- lib/days/Day12.ex
+++ lib/days/Day12.ex
@@ -1,16 +1,34 @@
 defmodule Aoc2021Ex.Day12 do
   use Aoc2021Ex.Day
+  @start_path [{{:small, "start"}, MapSet.new(), false}]
+  @end_cave {:small, "end"}
+  def solve1, do: count_paths(parsed_input(), false)
+  def solve2, do: count_paths(parsed_input(), true)
 
-  @start_path [{[{:small, "start"}], false}]
-  def solve1 do
-    expand(parsed_input(), @start_path, false, [])
-    |> length()
+  def count_paths(map, can_repeat?, paths \\ @start_path, acc \\ 0)
+  def count_paths(_, _, [], acc), do: acc
+
+  def count_paths(map, can_repeat?, _paths = [path = {cur, visits, repeated?} | rest], acc) do
+    {acc, paths} =
+      for pos <- Map.get(map, cur), can_visit?(pos, path, can_repeat?), reduce: {acc, rest} do
+        {acc, paths} ->
+          if pos == @end_cave do
+            {acc + 1, paths}
+          else
+            new_path =
+              {pos, MapSet.put(visits, pos), repeated? || (is_small?(pos) && pos in visits)}
+
+            {acc, [new_path | paths]}
+          end
+      end
+
+    count_paths(map, can_repeat?, paths, acc)
   end
 
-  def solve2 do
-    expand(parsed_input(), @start_path, true, [])
-    |> length()
-  end
+  def is_small?({size, _}), do: size == :small
+  def can_visit?({:big, _}, _, _), do: true
+  def can_visit?({:small, _}, {_, _, false}, true), do: true
+  def can_visit?(pos, {_, visited, _}, _), do: !MapSet.member?(visited, pos)
 
   def parsed_input do
     input_lines()
@@ -18,45 +36,11 @@ defmodule Aoc2021Ex.Day12 do
     |> Enum.reduce(%{}, fn [a, b], map ->
       for {x, y} <- [{a, b}, {b, a}], x != "end", y != "start", reduce: map do
         map ->
-          source = case_pair(x)
-          dest = case_pair(y)
+          {source, dest} = {case_pair(x), case_pair(y)}
           Map.update(map, source, [dest], &[dest | &1])
       end
     end)
   end
 
-  def expand(_, [], _, acc), do: acc
-
-  def expand(
-        transitions,
-        _paths = [path = {path_positions = [pos | _], has_double?} | rest],
-        allow_double?,
-        acc
-      ) do
-    next_positions =
-      Map.get(transitions, pos)
-      |> Enum.filter(fn next_pos -> can_visit?(next_pos, path, allow_double?) end)
-
-    {terminals, new_paths} =
-      Enum.map(next_positions, fn next_pos ->
-        {[next_pos | path_positions],
-         has_double? || (is_small?(next_pos) && next_pos in path_positions)}
-      end)
-      |> Enum.split_with(fn {[last_pos | _], _} -> last_pos == {:small, "end"} end)
-
-    expand(transitions, rest ++ new_paths, allow_double?, terminals ++ acc)
-  end
-
-  def is_small?({size, _}), do: size == :small
-  def can_visit?({:big, _}, _, _), do: true
-  def can_visit?({:small, _}, {_, false}, true), do: true
-  def can_visit?(pos, {path, _}, _), do: pos not in path
-
-  def case_pair(s) do
-    if String.downcase(s) == s do
-      {:small, s}
-    else
-      {:big, s}
-    end
-  end
+  def case_pair(s), do: if(String.downcase(s) == s, do: {:small, s}, else: {:big, s})
 end
