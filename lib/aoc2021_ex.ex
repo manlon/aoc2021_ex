dcbc6123ebc7bf447c063bb9b5b58d49a74b8a18
Matt Hanlon <matthanlon@gmail.com>
2021-12-17 23:19:13 -0800

    solve_all boilerplate

--- lib/aoc2021_ex.ex
+++ lib/aoc2021_ex.ex
@@ -2,6 +2,30 @@ defmodule Aoc2021Ex do
   @moduledoc """
   Documentation for `Aoc2021Ex`.
   """
+  use Aoc2021Ex.Day
+
+  def solve_all do
+    [
+      Aoc2021Ex.Day01.solve,
+      Aoc2021Ex.Day02.solve,
+      Aoc2021Ex.Day03.solve,
+      Aoc2021Ex.Day04.solve,
+      Aoc2021Ex.Day05.solve,
+      Aoc2021Ex.Day06.solve,
+      Aoc2021Ex.Day07.solve,
+      Aoc2021Ex.Day08.solve,
+      Aoc2021Ex.Day09.solve,
+      Aoc2021Ex.Day10.solve,
+      Aoc2021Ex.Day11.solve,
+      Aoc2021Ex.Day12.solve,
+      Aoc2021Ex.Day13.solve,
+      Aoc2021Ex.Day14.solve,
+      Aoc2021Ex.Day15.solve,
+      Aoc2021Ex.Day16.solve,
+      Aoc2021Ex.Day17.solve,
+      Aoc2021Ex.Day18.solve,
+    ]
+  end
 
   def make_module(day) do
     num = String.pad_leading(Integer.to_string(day), 2, "0")
